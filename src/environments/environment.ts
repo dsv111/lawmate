42e3526557f6393d468234393b63283717f3953d
dsv111 <srinivas.dasari2019@gmail.com>
2025-09-27 12:50:05 +0530

    api key updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   // geminiApiKey: 'AIzaSyD4VP1f8OEWzm0-6tQbGOkgLoxbla1KKKs', // **DO NOT COMMIT THIS TO GIT FOR PROD KEYS!**
-  // geminiApiKey: 'AIzaSyC55CdCA72hyy8OSVIcN6LCOG6oA0HLHpY', // **DO NOT COMMIT THIS TO GIT FOR PROD KEYS!**
-  geminiApiKey: 'AIzaSyD8DO5nrlcSttqbx3Jjc3oHEnPKJQqoUbQ', // **nanigaadu1234@gmil.com**
+  geminiApiKey: 'AIzaSyC8zKMDzg-DQpe0jK54xFb703APJ8VHNIo', // **DO NOT COMMIT THIS TO GIT FOR PROD KEYS!**
+  // geminiApiKey: 'AIzaSyD8DO5nrlcSttqbx3Jjc3oHEnPKJQqoUbQ', // **nanigaadu1234@gmil.com**
 
 };
